63865028ef28c09e0cf074e8819589fb54a48c58
Vishal R Setty <108011953+vishalsetty11@users.noreply.github.com>
2026-03-04 12:55:00 +0530

    Time: 534 ms (59.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT u.user_id AS buyer_id, join_date, COUNT(o.order_id) AS orders_in_2019
+FROM Users u
+LEFT JOIN Orders o ON u.user_id = o.buyer_id
+AND o.order_date BETWEEN '2019-01-01' AND '2019-12-31'
+GROUP BY u.user_id, u.join_date
+ORDER BY u.user_id;
