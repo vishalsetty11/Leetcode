69eece1fc9a40c433f82b6fcb16f97ac55d5a73f
Vishal R Setty <108011953+vishalsetty11@users.noreply.github.com>
2026-04-21 11:57:54 +0530

    Time: 260 ms (33.19%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,13 +1,9 @@
 -- Write your PostgreSQL query statement below
-WITH processing AS (
-    SELECT e.name AS EmpName,
-        e.salary AS Salary,
-        d.name AS DeptName,
-        DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS salRank
+SELECT t.dept_name AS Department, t.emp_name AS Employee, t.emp_sal AS Salary
+FROM (
+    SELECT e.name AS emp_name, e.salary AS emp_sal, d.name AS dept_name,
+        DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS sal_rnk_dept
     FROM Employee e
     JOIN Department d ON e.departmentId = d.id
-) 
-
-SELECT DeptName AS Department,EmpName AS Employee, Salary 
-FROM processing
-WHERE salRank = 1
+) AS t
+WHERE sal_rnk_dept = 1
